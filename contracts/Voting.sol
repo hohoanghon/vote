7f9106fa3cc5fd3ed88829f7997d4c4f77d262d4
hohoanghon <81347144+hohoanghon@users.noreply.github.com>
2022-03-15 11:41:30 +0700

    update file voting

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,11 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-contract Voting is Ownable {
+contract Voting is Ownable, ERC20 {
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
     address public numberVote; 
     Counters.Counter private counter;
+    constructor() ERC20 ("ACBank", "ACB")
+    {
+      _mint(msg.sender, 100000000000000000000000000);
+    }
     struct Topic
     {
       uint startTimeVote;
@@ -17,27 +21,32 @@ contract Voting is Ownable {
       uint id;  
     }
     Topic[] public topicArray;
+    function setTokenVote (address addressContract) external onlyOwner
+    {
+      numberVote = addressContract;
+    }
     function createTopic (uint256 startTime, uint endTime) external onlyOwner{
       Topic memory topic;
       topic.startTimeVote = startTime;
       topic.endTimeVote = endTime;
       topic.id = counter.current();
+      topicArray.push(topic);
       counter.increment();
     }
     function viewTopic(uint256 Index) public view returns(Topic memory topic){
-        topic = topicArray[Index];
+      topic = topicArray[Index];
     }
-
     function voteAgree(uint256 Index, uint256 amount) external {
       Topic storage topic = topicArray[Index];
       require(topic.endTimeVote >= block.timestamp, "Topic da dong");
       require(IERC20(numberVote).balanceOf(msg.sender) >= amount, "Khong du tai san de vote");
-      IERC20(numberVote).safeTransferFrom(msg.sender, address(this), amount);  
+      IERC20(numberVote).safeTransferFrom(msg.sender, address(this), amount);   
   }
-    function voteDontAgree(uint256 Index, uint256 amount) external{
-      Topic storage topic = topicArray[Index];
-      require(topic.endTimeVote >= block.timestamp, "Topic da dong");
-      require(IERC20(numberVote).balanceOf(msg.sender) >= amount, "Khong du tai san de vote");
-      IERC20(numberVote).safeTransferFrom(msg.sender, address(this), amount);    
-    }
+
+  function voteDontAgree(uint256 Index, uint256 amount) external{
+    Topic storage topic = topicArray[Index];
+    require(topic.endTimeVote >= block.timestamp, "Topic da dong");
+    require(IERC20(numberVote).balanceOf(msg.sender) >= amount, "Khong du tai san de vote");
+    IERC20(numberVote).safeTransferFrom(msg.sender, address(this), amount);    
+  }
 }
